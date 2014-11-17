089f381605d3be152772ab7a7865953d27e85e7f
Reto Schläpfer <reto.schlaepfer.rs@gmail.com>
2014-11-17 17:21:55 +0100

    slider scroller

--- src/components/NameList.jsx
+++ src/components/NameList.jsx
@@ -1,4 +1,5 @@
 require('./NameList.scss');
+var assign = require('lodash-node/modern/objects/assign');
 var React = require('react');
 var FilteredNamesStore = require('../stores/FilteredNamesStore');
 
@@ -6,23 +7,38 @@ function getState() {
     return FilteredNamesStore.getState();
 }
 
+var ELEMENT_HEIGHT = 16;
+
 var NameList = React.createClass({
     mixins: [FilteredNamesStore.mixin],
     getInitialState() {
-        return getState();
+        return assign({visStart: 0, visEnd: 100}, getState());
     },
     onChange() {
         this.setState(getState());
     },
     renderNames() {
-        return this.state.list.slice(0, 50).map((item) => {
-            return <div key={item.name}>{`${item.name} (${item.totalCount})`}</div>;
+        return this.state.list.slice(this.state.visStart, this.state.visEnd).map((item, i) => {
+            var top = (this.state.visStart + i) * ELEMENT_HEIGHT;
+            return (
+                <div key={item.name} style={{top: top}}>
+                    {`${item.name} (${item.totalCount})`}
+                </div>
+            );
+        });
+    },
+    setVisibleRange(e) {
+        var scrollTop = e.currentTarget.scrollTop;
+        var height = e.currentTarget.getBoundingClientRect().height;
+        this.setState({
+            visStart: Math.max(Math.round(scrollTop / ELEMENT_HEIGHT - 5), 0),
+            visEnd: Math.min(Math.round((scrollTop + height) / ELEMENT_HEIGHT + 5), this.state.list.length)
         });
     },
     render() {
         return (
-            <div className="namelist">
-                <div className="names-container" style={{height: 16 * this.state.list.length}}>
+            <div className="namelist" onScroll={this.setVisibleRange}>
+                <div className="names-container" style={{height: ELEMENT_HEIGHT * this.state.list.length}}>
                     {this.renderNames()}
                 </div>
             </div>

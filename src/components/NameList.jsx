10ee6497da852d3532b65c42bbae96f0d20e22ae
Reto Schläpfer <reto.schlaepfer.rs@gmail.com>
2014-11-17 20:02:08 +0100

    good scrolling

--- src/components/NameList.jsx
+++ src/components/NameList.jsx
@@ -12,32 +12,41 @@ var ELEMENT_HEIGHT = 16;
 var NameList = React.createClass({
     mixins: [FilteredNamesStore.mixin],
     getInitialState() {
-        return assign({visStart: 0, visEnd: 100}, getState());
+        return assign({scrollTop: 0}, getState());
     },
     onChange() {
         this.setState(getState());
     },
+    setScrollTop(e) {
+        this.setState({
+            scrollTop: e.currentTarget.scrollTop,
+        });
+    },
+    getVisibleRange() {
+        if (this.state.list.length) {
+            var start = Math.max((Math.round(this.state.scrollTop  / ELEMENT_HEIGHT) - 5), 0);
+            var height = this.refs.namelist.getDOMNode().clientHeight;
+            var end = Math.min(start + Math.round(height / ELEMENT_HEIGHT) + 10, this.state.list.length - 1);
+            return [start, end];
+        }
+        return [0, 0];
+    },
     renderNames() {
-        return this.state.list.slice(this.state.visStart, this.state.visEnd).map((item, i) => {
-            var top = (this.state.visStart + i) * ELEMENT_HEIGHT;
+        var range = this.getVisibleRange();
+        return this.state.list.slice(range[0], range[1]).map((item, i) => {
             return (
-                <div key={item.name} style={{top: top}}>
+                <div
+                 className="namelist-item"
+                 key={item.name}
+                 style={{top: (range[0] + i) * ELEMENT_HEIGHT}}>
                     {`${item.name} (${item.totalCount})`}
                 </div>
             );
         });
     },
-    setVisibleRange(e) {
-        var scrollTop = e.currentTarget.scrollTop;
-        var height = e.currentTarget.getBoundingClientRect().height;
-        this.setState({
-            visStart: Math.max(Math.round(scrollTop / ELEMENT_HEIGHT - 5), 0),
-            visEnd: Math.min(Math.round((scrollTop + height) / ELEMENT_HEIGHT + 5), this.state.list.length)
-        });
-    },
     render() {
         return (
-            <div className="namelist" onScroll={this.setVisibleRange}>
+            <div className="namelist" onScroll={this.setScrollTop} ref="namelist">
                 <div className="names-container" style={{height: ELEMENT_HEIGHT * this.state.list.length}}>
                     {this.renderNames()}
                 </div>
